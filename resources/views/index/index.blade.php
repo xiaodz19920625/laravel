d87a789987f559329418ac368a72dc6f9398b54c
xiaodz19920625 <1455688220@qq.com>
2020-09-22 10:38:30 +0800

    添加导航

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.default')
 @section('title','首页')
 @section('content')
-	<h1>首页</h1>
 @stop
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,10 +2,22 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>美莱-@yield('title','美莱')</title>
-	<link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+	<title>美莱-@yield('title','')</title>
+	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-	@yield('content')
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+			<a href="/" class="navbar-brand">美莱</a>
+			<ul class="navbar-nav justify-content-end">
+				<li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+				<li class="nav-item"><a href="javascript:;" class="nav-link">登陆</a></li>
+				<li class="nav-item"><a href="javascript:;" class="nav-link">注册</a></li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container">
+		@yield('content')
+	</div>
 </body>
 </html>
