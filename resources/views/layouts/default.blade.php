14b5680946275a6292e3426736abcf414aa9ad47
xiaodz19920625 <1455688220@qq.com>
2020-09-22 10:17:34 +0800

    首次提提交

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>美莱-@yield('title','美莱')</title>
+	<link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
